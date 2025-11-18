dfb054fe9218893009c1291f9cef888e2679a8e6
madhavkumaryadav <ymadhav081@gmail.com>
2025-11-18 19:29:54 +0530

    handle the value error

--- application.py
+++ application.py
@@ -12,26 +12,33 @@ standard_scaler = pickle.load(open(os.path.join(BASE, 'models/scaler.pkl'), 'rb'
 
 @app.route("/", methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method == "POST":
-        Temperature = float(request.form.get('Temperature'))
-        RH = float(request.form.get('RH'))
-        Ws = float(request.form.get('Ws'))
-        Rain = float(request.form.get('Rain'))
-        FFMC = float(request.form.get('FFMC'))
-        DMC = float(request.form.get('DMC'))
-        ISI = float(request.form.get('ISI'))
-        Classes = float(request.form.get('Classes'))
-        Region = float(request.form.get('Region'))
+    
+    try:
+    
+        if request.method == "POST":
+            Temperature = float(request.form.get('Temperature'))
+            RH = float(request.form.get('RH'))
+            Ws = float(request.form.get('Ws'))
+            Rain = float(request.form.get('Rain'))
+            FFMC = float(request.form.get('FFMC'))
+            DMC = float(request.form.get('DMC'))
+            ISI = float(request.form.get('ISI'))
+            Classes = float(request.form.get('Classes'))
+            Region = float(request.form.get('Region'))
 
-        # Prepare data
-        input_data = np.array([[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, Classes, Region]])
-        scaled = standard_scaler.transform(input_data)
+            # Prepare data
+            input_data = np.array([[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, Classes, Region]])
+            scaled = standard_scaler.transform(input_data)
 
-        result = ridge_model.predict(scaled)
+            result = ridge_model.predict(scaled)
 
-        return render_template('home.html', results=float(result[0]))
-    else:
-        return render_template('home.html')
+            return render_template('home.html', results=float(result[0]))
+    
+    
+        else:
+            return render_template('home.html')
+    except ValueError:
+        return render_template('error.html')
 
 if __name__ == "__main__":
     application.run(debug=True)
